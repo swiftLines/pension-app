5a0dde5084687113be8c5485565b3d120d70f1d9
Jeremy Underwood <underwood.j248@gmail.com>
2025-10-05 12:32:21 -0400

    add config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+class Config:
+    SECRET_KEY= os.getenv("SECRET_KEY", "change-me")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # where logs will be written (app creates this on startup if needed)
+    LOG_DIR = os.path.join(os.path.dirname(__file__), 'logs')
