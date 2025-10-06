4e472231034040c973361e26f91fd7631a8f7c02
Jeremy Underwood <underwood.j248@gmail.com>
2025-10-06 11:23:52 -0400

    add logging

--- app/__init__.py
+++ app/__init__.py
@@ -14,4 +14,18 @@ csrf = CSRFProtect()
 def create_app():
     app = Flask(__name__)
     app.config.from_object('config.Config')
+
+    # Extensions
+    db.init_app(app)
+    login_manager.init_app(app)
+    csrf.init_app(app)
+    login_manager.login_view = 'auth.login'
+
+    # Logging
+    log_dir = app.config['LOG_DIR']
+    os.makedirs(log_dir, exist_ok=True)
+    file_handler = RotatingFileHandler(os.path.join(log_dir, 'app.log'), maxBytes=1_000_000, backupCount=3)
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(logging.Formatter('[%(asctime)s] %(levelname)s in %(module)s: %(message)s'))
+    app.logger.addHandler(file_handler)
     
